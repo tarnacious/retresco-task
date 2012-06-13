00b7c846ccc91c6cef1df85715d70184ce168756
tarn <tarn.barford@gmail.com>
2012-06-13 20:48:12 +1000

    Added counting monthly unique views. Split up tests into separate classes

--- bitmask_count.py
+++ bitmask_count.py
@@ -6,18 +6,28 @@ class ArticleViews:
     def __init__(self, redis):
 
         self.redis = redis
+        self.key_format = "views:%(document_name)s:%(year)s:%(month)s:%(day)s"
 
 
     def document_key(self, document_name, date):
 
-        key_format = "views:%(document_name)s:%(year)d:%(month)d:%(day)d"
+        key_values = { "document_name": document_name,
+                       "day": str(date.day),
+                       "month": str(date.month),
+                       "year": str(date.year) }
+        
+        return self.key_format % key_values
+
+
+    def document_month_key(self, document_name, month, year):
         
         key_values = { "document_name": document_name,
-                       "day": date.day,
-                       "month": date.month,
-                       "year": date.year }
+                       "day": "*",
+                       "month": str(month),
+                       "year": str(year) }
         
-        return key_format % key_values
+        return self.key_format % key_values
+    
 
 
     def view_article(self, document_name, user_key, date):
@@ -31,12 +41,33 @@ class ArticleViews:
 
         key = self.document_key(document_name, date)
 
-        key_value = self.redis.get(key)
+        return self.count_views(key)
 
-        if key_value == None:
-            return 0
+    
+    def article_monthly_views(self, document_name, month, year):
 
-        bits = bitarray() 
-        bits.frombytes(key_value)
-        return bits.count()
+        key = self.document_month_key(document_name, month, year)
+
+        return self.count_views(key)
+
+
+    def count_views(self, document_keys): 
+        
+        keys = self.redis.keys(document_keys)
+
+        union = bitarray()
+        
+        for key in keys:
+
+            key_value = self.redis.get(key)
+
+            if not key_value == None:
+                bits = bitarray() 
+                bits.frombytes(key_value)
+
+                if bits.length() > union.length():
+                    union.extend(list(0 for n in range(bits.length() - union.length())))
+                union = union | bits 
+               
+        return union.count()
 
--- bitmask_count_test.py
+++ bitmask_count_test.py
@@ -3,7 +3,46 @@ from datetime import datetime
 import bitmask_count
 import redis
 
-class testbitmask_count(unittest.TestCase):
+class testKeys(unittest.TestCase):
+
+    """
+    
+    Test the generation of keys
+
+    """
+    
+    def setUp(self):
+
+        self.article_views = bitmask_count.ArticleViews(None) 
+
+    
+    def testDocumentKey(self):
+
+        date = datetime(2012,6,12)
+        key = self.article_views.document_key("test_document", date) 
+
+        expected = "views:test_document:2012:6:12"
+       
+        self.assertEqual(expected, key)
+    
+    
+    def testDocumentMonthKey(self):
+
+        key = self.article_views.document_month_key("test_document", 6, 2012) 
+
+        expected = "views:test_document:2012:6:*"
+       
+        self.assertEqual(expected, key)
+
+
+
+class testCountingDailyDocumentViews(unittest.TestCase):
+
+    """
+    
+    Test viewing and counting unique views of a single document
+
+    """
 
     def setUp(self):
 
@@ -15,16 +54,6 @@ class testbitmask_count(unittest.TestCase):
             self.redis.delete(key)
 
         self.article_views = bitmask_count.ArticleViews(self.redis) 
-
-
-    def testDocumentKey(self):
-
-        date = datetime(2012,6,12)
-        key = self.article_views.document_key("test_document", date) 
-
-        expected = "views:test_document:2012:6:12"
-       
-        self.assertEqual(expected, key)
 
 
     def testEmptyKeyHasZeroViews(self):
@@ -73,6 +102,60 @@ class testbitmask_count(unittest.TestCase):
         views = self.article_views.article_views("test_document", date)
 
         self.assertEqual(2, views)
+
+
+class testCountMonthlyDocumentViews(unittest.TestCase):
+
+    """
+    
+    Test viewing and counting unique views of a single document
+
+    """
+
+    def setUp(self):
+
+        self.redis = redis.Redis()
+        
+        # Delete test keys
+        test_keys = self.redis.keys("views:*")
+        for key in test_keys:
+            self.redis.delete(key)
+
+        self.article_views = bitmask_count.ArticleViews(self.redis) 
+
+
+    def testMonthHasZeroViews(self):
+        
+        date = datetime(2012,6,12)
+        views = self.article_views.article_monthly_views("test_document", 6, 2012)
+
+        self.assertEqual(0, views)
+    
+
+    def testMultipleUniqueViews(self):
+        
+        # view article
+        self.article_views.view_article("test_document", 1, datetime(2012,6,12))
+        self.article_views.view_article("test_document", 5, datetime(2012,6,14))
+        
+        # get views
+        views = self.article_views.article_monthly_views("test_document", 6, 2012)
+
+        self.assertEqual(2, views)
+
+    
+    def testDuplicateView(self):
+        
+        # view article
+        self.article_views.view_article("test_document", 5, datetime(2012,6,12))
+        self.article_views.view_article("test_document", 5, datetime(2012,6,14))
+        
+        # get views
+        views = self.article_views.article_monthly_views("test_document", 6, 2012)
+
+        self.assertEqual(1, views)
+
+
 
 if __name__ == '__main__':
 
